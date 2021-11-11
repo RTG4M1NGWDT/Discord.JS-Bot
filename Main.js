51b4d2fa64a89a231f7664a169fe4f593d6a7966
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-11 16:56:19 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,15 +1,20 @@
-const { Client, Intents } = require('discord.js');
-const { TOKEN } = process.env;
+const {
+    Client,
+    Intents
+} = require('discord.js');
+const {
+    TOKEN
+} = process.env;
 
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES
+    intents: [
+        Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES
     ]
 });
 
 client.on('ready', () => {
-  console.log(`${client.user.username} is online!`);
-  require('./handlers/EventHandler')(client);
+    console.log(`${client.user.username} is online!`);
+    require('./handlers/EventHandler')(client);
 });
 
 client.login(TOKEN);
