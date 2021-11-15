7bc22568b4da97a895d65855f66c81350d7f722c
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-14 19:31:46 -0500

    Made code smaller

--- Main.js
+++ Main.js
@@ -1,27 +1,4 @@
-const {
-    Client,
-    Collection,
-    Intents
-} = require('discord.js');
-const {
-    TOKEN
-} = process.env;
+const ClientManager = require('./Manager/ClientManager');
 
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ]
-});
-
-client.commands = new Collection();
-client.aliases = new Collection();
-client.queue = new Map();
-
-client.on('ready', () => {
-    console.log(`${client.user.username} is online!`);
-    require('./handlers/EventHandler.js')(client);
-    require('./handlers/CommandHandler.js')(client);
-});
-
-client.login(TOKEN);
+client = new ClientManager();
+client.build();
