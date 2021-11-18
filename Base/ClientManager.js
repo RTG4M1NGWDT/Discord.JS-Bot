5ebe91d26ac8dd0875c9f5332da596a6a0b5b8da
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-18 07:43:47 -0500

    Update ClientManager.js

--- Base/ClientManager.js
+++ Base/ClientManager.js
@@ -1,19 +1,10 @@
-const { 
-  Client,
-  Collection,
-  Intents
-  } = require('discord.js');
-const {
-  readdirSync
-  } = require('fs');
+const { Client, Collection, Intents } = require("discord.js");
+const { readdirSync } = require("fs");
 
 class ClientManager extends Client {
   constructor(options = {}) {
     super({
-      intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-      ]
+      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
     });
 
     this.commands = new Collection();
@@ -26,28 +17,29 @@ class ClientManager extends Client {
     this.eventHandler();
     this.commandHandler();
   }
-  
-  async eventHandler() {
-    const folder = readdirSync("./Events");
 
-    for(const file of folder) {
-      const event = new (require(`../Events/${file}`));
+  async eventHandler() {
+    const folder = readdirSync("./events");
+
+    for (const file of folder) {
+      const event = new (require(`../events/${file}`))();
       this.on(event.name, event.listen.bind(null, this));
-      console.log(event)
     }
   }
 
   async commandHandler() {
-    const folders = readdirSync("./Commands");
+    const folders = readdirSync("./commands");
 
-    for(const folder of folders) {
-      const files = readdirSync(`./Commands/${folder}`).filter((file) => file.endsWith('.js'));
+    for (const folder of folders) {
+      const files = readdirSync(`./commands/${folder}`).filter(file =>
+        file.endsWith(".js")
+      );
 
-      for(const file of files) {
-        const command = new (require(`../Commands/${folder}/${file}`));
-        this.commands.set(command.name, command);
+      for (const file of files) {
+        const command = new (require(`../commands/${folder}/${file}`))();
+        this.commands.set(command.getName(), command);
 
-        for(const alias of command.aliases) {
+        for (const alias of command.getAliases()) {
           this.aliases.set(alias, command);
         }
       }
