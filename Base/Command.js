5866b56c9d7a4d11cdd47487307f4dd8aeb06284
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-18 07:44:13 -0500

    Update Command.js

--- Base/Command.js
+++ Base/Command.js
@@ -1,17 +1,84 @@
 class Command {
   constructor(options = {}) {
     this.name = options.name;
-    this.aliases = options.aliases || [];
-    this.description = options.description || "No description provided";
-    this.usage = options.usage || "No usage provided";
+    this.permission = options.permission;
+    this.description = options.description;
+    this.usage = options.usage;
+    this.aliases = options.aliases;
+    this.minArgs = options.minArgs;
+    this.maxArgs = options.maxArgs;
   }
 
   async execute(client, message, args) {
     throw new Error(`Could not find execute method in command ${this.name}`);
   }
 
-  async getName() {
+  getName() {
     return this.name;
+  }
+
+  getPermission() {
+    return this.permission;
+  }
+
+  setPermission(permission) {
+    this.permissions.push(permission);
+  }
+
+  unsetPermission() {
+    this.permissions.pop();
+  }
+
+  getPermissionMessage() {
+    return this.permissionMessage;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  getUsage() {
+    return this.usage;
+  }
+
+  setAliases(alias) {
+    this.aliases.push(alias);
+  }
+
+  unsetAliases() {
+    this.aliases.pop();
+  }
+
+  getAliases() {
+    return this.aliases || [];
+  }
+
+  setDescription(description) {
+    this.description = description;
+  }
+
+  setPermissionMessage(permissionMessage) {
+    this.permissionMessage = permissionMessage;
+  }
+
+  setUsage(usage) {
+    this.usage = usage;
+  }
+
+  getDescription() {
+    return this.description || "No description provided";
+  }
+
+  getUsage() {
+    return this.usage || "No usage provided";
+  }
+
+  getMinArgs() {
+    return this.minArgs || 0;
+  }
+
+  getMaxArgs() {
+    return this.maxArgs || null;
   }
 }
 
