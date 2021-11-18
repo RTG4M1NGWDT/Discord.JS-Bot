f88c4f4fbb67215fb4b12e1b10609b2bcc20d0a2
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-18 07:44:40 -0500

    Update Event.js

--- Base/Event.js
+++ Base/Event.js
@@ -1,14 +1,16 @@
 module.exports = class Event {
+  constructor(options = {}) {
+    this.name = options.name;
+  }
 
-	constructor(options = {}) {
-		this.name = options.name;
-		this.client = options.client;
-		this.type = options.once ? 'once' : 'on';
-		this.emitter = (typeof options.emitter === 'string' ? this.client[options.emitter] : options.emitter) || this.client;
-	}
+  // eslint-disable-next-line no-unused-vars
+  async listen(...args) {
+    throw new Error(
+      `The listen method has not been implemented in ${this.name}`
+    );
+  }
 
-	// eslint-disable-next-line no-unused-vars
-	async listen(...args) {
-		throw new Error(`The run method has not been implemented in ${this.name}`);
-	}
+  getName() {
+    return this.name;
+  }
 };
