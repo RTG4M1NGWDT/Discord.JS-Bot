57ebc215e5716e893f9c8fc21845bc3939d3944a
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-18 07:48:13 -0500

    Create Aww.js

---
+++ commands/animals/Aww.js
@@ -0,0 +1,51 @@
+const Command = require("../../base/Command");
+const got = require("got");
+const { MessageEmbed } = require("discord.js");
+
+class Aww extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: "aww",
+      aliases: [],
+      description: "See some random cute animals",
+      usage: "!aww",
+      category: "Animals"
+    });
+  }
+
+  async execute(client, message, args) {
+    const subreddits = ["aww"];
+    const index = Math.floor(
+      Math.random() * (subreddits.length - 1 - 0 + 1) + 0
+    );
+    const embed = new MessageEmbed();
+    await got(
+      `https://www.reddit.com/r/${subreddits[index]}/random/.json?sort=top&t=day&limit=100`
+    )
+      .then(response => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
+
+        const permalink = post.data.permalink;
+        const animalUrl = `https://reddit.com${permalink}`;
+        const animalImage = post.data.preview.images[0].source.url.replace(
+          new RegExp("amp;", "g"),
+          ""
+        );
+        const animalTitle = post.data.title;
+        const animalUpvotes = post.data.ups;
+        const animalNumComments = post.data.num_comments;
+
+        embed.setTitle(`${animalTitle}`);
+        embed.setURL(`${animalUrl}`);
+        embed.setColor("RANDOM");
+        embed.setImage(animalImage);
+        embed.setFooter(`👍 ${animalUpvotes} 💬 ${animalNumComments}`);
+
+        message.channel.send({ embeds: [embed] });
+      })
+      .catch(console.error);
+  }
+}
+
+module.exports = Aww;
