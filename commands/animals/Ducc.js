0fb6fa41bd91d19a9d096e874cb677d146467738
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-11-18 07:48:54 -0500

    Create Ducc.js

---
+++ commands/animals/Ducc.js
@@ -0,0 +1,50 @@
+const Command = require("../../base/Command");
+const got = require("got");
+const { MessageEmbed } = require("discord.js");
+
+class Ducc extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: "ducc",
+      aliases: [],
+      description: "Quack quack!",
+      usage: "!ducc",
+      category: "Animals"
+    });
+  }
+
+  async execute(client, message, args) {
+    const subreddits = ["duck"];
+    const index = Math.floor(
+      Math.random() * (subreddits.length - 1 - 0 + 1) + 0
+    );
+    const embed = new MessageEmbed();
+    await got(
+      `https://www.reddit.com/r/${subreddits[index]}/random/.json?sort=top&t=day&limit=100`
+    ).then(response => {
+      const [list] = JSON.parse(response.body);
+      const [post] = list.data.children;
+
+      const permalink = post.data.permalink;
+      const animalUrl = `https://reddit.com${permalink}`;
+      const animalImage = post.data.preview.images[0].source.url.replace(
+        new RegExp("amp;", "g"),
+        ""
+      );
+      if (!animalImage) return;
+      const animalTitle = post.data.title;
+      const animalUpvotes = post.data.ups;
+      const animalNumComments = post.data.num_comments;
+
+      embed.setTitle(`${animalTitle}`);
+      embed.setURL(`${animalUrl}`);
+      embed.setColor("RANDOM");
+      embed.setImage(animalImage);
+      embed.setFooter(`👍 ${animalUpvotes} 💬 ${animalNumComments}`);
+
+      message.channel.send({ embeds: [embed] });
+    });
+  }
+}
+
+module.exports = Ducc;
